db411f18447b6c6b3dccb0d3b2a46efa5b815a31
polina.nosova <polina.nosova@yalantis.net>
2023-04-14 16:45:32 +0300

    feat:calculator

---
+++ calculator.js
@@ -0,0 +1,24 @@
+let expression = '';
+let result = '';
+
+function updateExpression(value) {
+  expression += value;
+  document.getElementById('expression').value = expression;
+}
+
+function clearExpression() {
+  expression = '';
+  result = '';
+  document.getElementById('expression').value = expression;
+  document.getElementById('result').value = result;
+}
+
+function evaluateExpression() {
+  try {
+    result = eval(expression);
+    document.getElementById('result').value = result;
+  } catch (error) {
+    result = 'Error';
+    document.getElementById('result').value = result;
+  }
+}
